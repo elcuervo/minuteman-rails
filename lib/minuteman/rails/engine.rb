0ca8665b749142668c263c7047de7168d2047a1f
Bruno Aguirre <yo@brunoaguirre.com>
2012-11-29 01:08:52 -0200

    Fix helper namespace

--- lib/minuteman/rails/engine.rb
+++ lib/minuteman/rails/engine.rb
@@ -5,8 +5,8 @@ class Minuteman
   class Rails
     class Engine < ::Rails::Engine
       initializer "minuteman-rails" do |app|
-        ActionController::Base.send :include, Minuteman::Helper
-        ActionController::Base.helper Minuteman::Helper
+        ActionController::Base.send :include, Minuteman::Rails::Helper
+        ActionController::Base.helper Minuteman::Rails::Helper
       end
     end
   end
